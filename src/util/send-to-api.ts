48a46a39f6cbe49e2252d83d2a29ff77d44b4b85
Fractal-tess <vgfractal@gmail.com>
2022-04-14 01:45:35 +0300

    Added throw statement

--- src/util/send-to-api.ts
+++ src/util/send-to-api.ts
@@ -2,8 +2,10 @@ import { ScrapeFiction } from '@types'
 import { API_HOST } from '../main.ts'
 
 export const sendToApi = async (fiction: ScrapeFiction): Promise<void | never> => {
-  await fetch(API_HOST + '/api/v1/fiction', {
+  const res = await fetch(API_HOST + '/api/v1/fiction', {
     method: 'POST',
     body: JSON.stringify(fiction)
   })
+
+  if (res.status !== 200) throw new Error(`Request resulted in ${res.status} status code`)
 }
