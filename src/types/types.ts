e89c226c43d5a2b35e9aa23ba693f63aca604795
Fractal-tess <vgfractal@gmail.com>
2022-04-14 01:45:47 +0300

    made useful type

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,6 +5,15 @@ export enum APIStatus {
 }
 
 type Asset = string | Uint8Array | null
+type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>
+  }[Keys]
+
+export type FictionContentScrape = RequireOnlyOne<
+  Partial<ScrapeFictionChapter>,
+  'content'
+>
 
 export interface ScrapeFiction {
   title: string
